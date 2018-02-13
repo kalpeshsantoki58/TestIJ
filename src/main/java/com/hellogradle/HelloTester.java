8d5dc5a647b8418b42c38078b047c032074ad78d
kalpesh <kalpesh.santoki@openxcell.info>
2018-02-13 09:38:20 +0000

    Added Tester class

---
+++ src/main/java/com/hellogradle/HelloTester.java
@@ -0,0 +1,10 @@
+package com.hellogradle;
+
+public class HelloTester {
+    private String msg;
+    public HelloTester(String msg){
+        this.msg=msg;
+    }
+
+    public void print(){System.out.println(msg);}
+}
--- src/main/java/com/hellogradle/Test.java
+++ src/main/java/com/hellogradle/Test.java
@@ -3,5 +3,6 @@ package com.hellogradle;
 public class Test {
     public static void main(String[] args){
         System.out.println("Hello Gradle!");
+        new HelloTester("Message testing...").print();
     }
 }
