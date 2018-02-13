8231b87cba6f84e346df8f89825c255c69d3921a
kalpesh <kalpesh.santoki@openxcell.info>
2018-02-13 09:31:11 +0000

    Updated Test.java

--- src/main/java/com/hellogradle/Test.java
+++ src/main/java/com/hellogradle/Test.java
@@ -2,6 +2,6 @@ package com.hellogradle;
 
 public class Test {
     public static void main(String[] args){
-        System.out.println("Hello!");
+        System.out.println("Hello Gradle!");
     }
 }
