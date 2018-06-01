7fa352ccf291f18d3b87086db0637300bf30ca5e
Kalpesh Santoki <kalpesh.santoki@openxcell.info>
2018-06-01 09:07:21 +0000

    User modifiedd 2

--- src/main/java/com/hellogradle/User.java
+++ src/main/java/com/hellogradle/User.java
@@ -3,6 +3,15 @@ package com.hellogradle;
 public class User {
     private int id;
     private String name;
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
     public int getId() {
         return id;
